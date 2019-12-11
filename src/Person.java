5c06f44542cb07bce0da3339e651725ae4fdcd35
benji <benji@172.31.122.192>
2019-12-11 15:12:29 -0500

    Updated the classes

--- src/Person.java
+++ src/Person.java
@@ -1,4 +1,24 @@
 
 public class Person {
-	private double account; 
+	private String name;
+	private int pin;
+	
+
+public Person(String name, int pin) {
+	this.name = name;
+	this.pin = pin;
+}
+
+public boolean checkPin(int putin){
+	if(putin == pin) {
+		return true;
+	}else {
+	
+	return false;
+	}
+}
+
+public int getPin() {
+	return pin;
+}
 }
--- src/bankAccount.java
+++ src/bankAccount.java
@@ -1,4 +1,14 @@
 
 public class bankAccount {
+private Person person;
+private double balance;
 
+public bankAccount(Person person, double balance) {
+	this.person = person;
+	this.balance = balance;
+}
+
+public double getBalance() {
+	return this.balance;
+}
 }
