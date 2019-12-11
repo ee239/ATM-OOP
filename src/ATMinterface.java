8015dd61a0dff983db1f1d24cb2eacf4595d42e5
benji <benji@172.31.122.192>
2019-12-11 15:31:35 -0500

    ATm

--- src/ATMinterface.java
+++ src/ATMinterface.java
@@ -1,9 +1,41 @@
+import java.util.Scanner;
 
 public class ATMinterface {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Person mike = new Person("Mike",1234);
+		
+		boolean a = false;
+		int pin = 0;
+		
+		while(!a) {
+		System.out.println("Welcome! Please enter your pin");
+		Scanner scanner = new Scanner(System.in);
+		
+		pin = scanner.nextInt();
+		if(!mike.checkPin(pin)) {
+			System.out.println("That pin is incorrect please try again");
+			continue;
+		}
+		a = true;
+		}
+		
+		System.out.println("What would you like to do?");
+		System.out.println("1 : Deposit");
+		System.out.println("2 : Withdrawl");
+		System.out.println("3 : Check Balance");
+		System.out.println("4 : Exit");
+		
+		System.out.print(pin);
+		
 	}
+	
+	public void deposit() {
+		
+		
+	}
+	
+	
 
 }
